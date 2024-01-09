ce37bbe15ad98eb7f48ae441a9c25c2574b595a2
mcapxx <chetanmahesh1620@gmail.com>
2024-01-09 15:37:39 +0530

    frontend completed

---
+++ script.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
+
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
